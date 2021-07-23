e4b0918db1c363f5ac5efe09b0b1a8f5fa0abc13
Stanley Moukhametzianov <Stanleymoukh@gmail.com>
2021-07-23 19:03:02 -0400

    Initial Commit

---
+++ Covid Data/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid Data
+//
+//  Created by Stanley Moukh on 7/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
